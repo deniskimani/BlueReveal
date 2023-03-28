b5905f2baed82cde21e48ef5d815ada93bce97ae
deniskimani <48308527+deniskimani@users.noreply.github.com>
2023-03-28 19:11:14 +0300

    deploy

---
+++ js/technologies.2b30a743.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["technologies"],{"0d67":function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("div",{staticClass:"breadcrumbs text-center"},[n("h2",[e._v("Technologies")])])])}],r={},l=r,i=n("2877"),a=Object(i["a"])(l,c,s,!1,null,null,null);t["default"]=a.exports}}]);
+//# sourceMappingURL=technologies.2b30a743.js.map
